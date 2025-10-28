d52d64d2e4b85bfe1dce5db675d826a8099f3729
Alef Carvalho de Melo <20221204010037@ifrn.local>
2025-10-28 11:50:38 -0300

    Inacabado da aula

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -86,5 +86,33 @@ public class EventosController {
 		
 		return "redirect:/eventos/{idEvento}";
 	}
-
+	@GetMapping("/{id}/remover")
+	public String apagarEvento(@PathVariable Long id) {
+		
+		Optional<Evento> opt = er.findById(id);
+		
+		if(!opt.isEmpty()) {
+			Evento evento = opt.get();
+			
+			
+			List<Convidado> convidados = cr.findByEvento(evento);
+			
+			cr.deleteAll(convidados);
+			er.delete(evento);
+		}
+		return "redirect:/eventos";
+	}
+	
+	@GetMapping("/convidado/{id}/remover")
+	public String apagarConvidado(@PathVariable Long id) {
+		
+		Optional<Convidado> opt = cr.findById(id);
+		
+		if(!opt.isEmpty()) {
+			Convidado convidado = opt.get();
+			cr.delete (convidado);
+		}
+		return "redirect:/eventos/{id}";
+	}
+	
 }
